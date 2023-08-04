d68eec24adde7f16675f3b90315bb112f091f2a7
md0727 <md.developer0727@gmail.com>
2023-08-04 19:52:55 +0530

    this is cbrs4 project

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,12 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
+import Navbar from '@/components/Navbar';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className='dark'>
       <Head />
       <body>
+        <Navbar />
         <Main />
         <NextScript />
       </body>
